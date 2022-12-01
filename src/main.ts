4e0305cb8a49172088a18d7a6f773eae0973ba3a
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-12-01 08:26:12 -0300

    chore: add envs config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import * as env from 'dotenv';
 import { AppModule } from './app.module';
 
+env.config();
+
 async function bootstrap() {
+  const port = process.env.APP_PORT || 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
+
 bootstrap();
