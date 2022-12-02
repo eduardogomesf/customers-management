08c2d45eef3b0709407683163d8f5a33ab83f4b1
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-12-02 10:13:17 -0300

    feat: add get customer by id repository

--- src/repositories/redis/redis-customer.repository.ts
+++ src/repositories/redis/redis-customer.repository.ts
@@ -2,9 +2,12 @@ import Redis from 'ioredis';
 
 import { Customer } from '../../models/customer';
 import { CreateCustomerRepository } from '../../useCases/protocols/create-customer.repository';
+import { GetCustomerByIdRepository } from '../../useCases/protocols/get-customer-by-id.repository';
 
-export class RedisCustomerRepository implements CreateCustomerRepository {
+export class RedisCustomerRepository implements CreateCustomerRepository, GetCustomerByIdRepository {
   private repository: Redis;
+
+  private keyPrefix = 'customer:';
 
   constructor() {
     this.repository = new Redis();
@@ -12,9 +15,16 @@ export class RedisCustomerRepository implements CreateCustomerRepository {
 
   async create(customer: Customer): Promise<void> {
     await this.repository.set(
-      `customer:${customer.id}`,
+      `${this.keyPrefix}${customer.id}`,
       JSON.stringify(customer),
     );
     return null;
+  }
+
+  async getById(customerId: string): Promise<Customer> {
+    const stringifiedCustomer = await this.repository.get(
+      `${this.keyPrefix}${customerId}`,
+    );
+    return stringifiedCustomer ? JSON.parse(stringifiedCustomer) : null;
   }
 }
