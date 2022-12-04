1577e3622ff871854433f79a2eb9a1f81443ff26
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-12-04 11:48:47 -0300

    chore: add env example file

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { CONFIGS } from './configs/envs';
 
 async function bootstrap() {
-  const port = CONFIGS.APP.PORT || 3000;
+  const port = CONFIGS.APP.PORT || 3333;
 
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
