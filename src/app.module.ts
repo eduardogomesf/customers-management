c5f1eeb688002f849acdef22817dcac5d6c030d2
Eduardo Gomes <eduardogoomesf@gmail.com>
2022-12-03 23:25:10 -0300

    feat: add keycloak integration to validate requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard, KeycloakConnectModule } from 'nest-keycloak-connect';
+import { CONFIGS } from './configs/envs';
 import { CustomerModule } from './modules/customer.module';
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: CONFIGS.KEYCLOAK.AUTH_SERVER_URL,
+      realm: CONFIGS.KEYCLOAK.REALM,
+      clientId: CONFIGS.KEYCLOAK.CLIENT_ID,
+      secret: CONFIGS.KEYCLOAK.SECRET,
+    }),
+    CustomerModule
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    }
+  ],
 })
 export class AppModule {}
